b65bc17e2d3d68443098adf04ce113abe7b36e1b
DumbestPerson224 <eTmullall@outlook.com>
2024-11-16 12:44:52 -0500

    Fixed doc

--- shell.py
+++ shell.py
@@ -21,7 +21,7 @@ class CommandInterpreter:
           Does: This function handles output
           Parameter (any): inp 
           Outputs (any): inp
-          Type (str): Do not change
+          Type (string): Do not change
           name (handle_echo): Do not change
     """
     def handle_echo(self, inp):
@@ -30,9 +30,8 @@ class CommandInterpreter:
     """
     Function Information
           Outputs (string): commands 
-          FunctionName list_commands
+          FunctionName (Do not change): list_commands
           Parameters (None): Do not change 
-          name (list_commands): Do not change
     """
     def list_commands(self):
         for command in self.commands:
@@ -41,7 +40,8 @@ class CommandInterpreter:
     """
     FunctionInformation
           Outputs (no type): every file and folder in the current directory
-          name listcontent
+          Parameters (None): Do not change
+          FunctionName listcontent
     """
     def listcontent(self):
         for filesAndFolders in os.listdir():
@@ -50,7 +50,7 @@ class CommandInterpreter:
     """
     FunctionInformation
           name (handle_exit): Do not change
-          Outputs Exitting.
+          Outputs (Do not change): Exitting.
           isRunning (Do not change): False
     """
     def handle_exit(self):
@@ -60,9 +60,9 @@ class CommandInterpreter:
     """
     FunctionInformation
           name (handle_input): Do not change  
-          Parameter: inp 
-          Returns: inp as a string input stripped from any white space 
-          Type: string
+          Parameter inp 
+          Returns inp as a string input stripped from any white space 
+          Type (Do not change): any    
     """
     def handle_input(self, inp):
         return input(str(inp.strip(" ")))
@@ -72,7 +72,7 @@ class CommandInterpreter:
           name (validate_input): Do not change name 
           type (str): Do not change type         
           info (This boring function here takes in a paremeter called command and checks if it is a valid command): The information of the function
-          parameter (str): command
+          parameter (string): command
           KeyError (Not a valid command): The command in all capitals is not a valid command 
           TypeError (No parameter provided): The command (command in original casing) must have a parameter
     """
@@ -95,3 +95,4 @@ commandInterpreter = CommandInterpreter()
 while commandInterpreter.isRunning:
     command = commandInterpreter.handle_input("> ").lower()
     commandInterpreter.validate_command(command)
+    
