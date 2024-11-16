722e8287a2087489eb492be7b99a17d284dfabfd
DumbestPerson224 <eTmullall@outlook.com>
2024-11-16 15:27:01 -0500

    Fixed issues

--- shell.py
+++ shell.py
@@ -15,84 +15,79 @@ class CommandInterpreter:
         "`isRunning` and `osMode` must only be booleans"
         self.isRunning = True
         self.osMode = False 
-        
-    """
-    FunctionInformation
-          Does: This function handles output
-          Parameter (any): inp 
-          Outputs (any): inp
-          Type (string): Do not change
-          name (handle_echo): Do not change
-    """
-    def handle_echo(self, inp):
+  
+    def handle_echo(self, inp):      
+        """
+        FunctionInformation
+            Does: This function handles output
+            Parameter (any): inp 
+            Outputs (any): inp
+            Type (string): Do not change
+            name (handle_echo): Do not change
+        """
         print(inp)
     
-    """
-    Function Information
+    
+    def list_commands(self):
+        """
+        Function Information
           Outputs (string): commands 
           FunctionName (Do not change): list_commands
           Parameters (None): Do not change 
-    """
-    def list_commands(self):
+        """
         for command in self.commands:
             print(command)
             
-    """
-    FunctionInformation
-          Outputs (no type): every file and folder in the current directory
-          Parameters (None): Do not change
-          FunctionName listcontent
-    """
     def listcontent(self):
+        """
+        FunctionInformation
+            Outputs (no type): every file and folder in the current directory
+            Parameters (None): Do not change
+            FunctionName listcontent
+        """
+         
         for filesAndFolders in os.listdir():
             print(filesAndFolders)
 
-    """
-    FunctionInformation
-          name (handle_exit): Do not change
-          Outputs (Do not change): Exitting.
-          isRunning (Do not change): False
-    """
+    
     def handle_exit(self):
+        """
+        FunctionInformation
+            name (handle_exit): Do not change
+            Outputs (Do not change): Exitting.
+            isRunning (Do not change): False
+        """
         print("Exiting..")
         self.isRunning = False
         
-    """
-    FunctionInformation
-          name (handle_input): Do not change  
-          Parameter inp 
-          Returns inp as a string input stripped from any white space 
-          Type (Do not change): any    
-    """
-    def handle_input(self, inp):
+   
+    def handle_input(self, inp):     
+        """
+        FunctionInformation
+            name (handle_input): Do not change  
+            Parameter inp 
+            Returns inp as a string input stripped from any white space 
+            Type (Do not change): any    
+        """
         return input(str(inp.strip(" ")))
         
-    """
-    FunctionInformation:
-          name (validate_input): Do not change name 
-          type (str): Do not change type         
-          info (This boring function here takes in a paremeter called command and checks if it is a valid command): The information of the function
-          parameter (string): command
-          KeyError (Not a valid command): The command in all capitals is not a valid command 
-          TypeError (No parameter provided): The command (command in original casing) must have a parameter
-    """
-    def validate_command(self, command):
-     
-        try:
+   
+    def validate_command(self, command) -> str:
+        """
+        FunctionInformation:
+            name (validate_input): Do not change name 
+            type (str): Do not change type         
+            info (This boring function here takes in a paremeter called command and checks if it is a valid command): The information of the function
+            parameter (string): command
+            NoValidCommand (output): (The user input in caps) is not valid
+        """
+        if command in self.commands and not command.startswith("echo"):
             self.commands[command]()
-     
-        except KeyError:
-     
-            if not command.startswith("echo"):
-                print(f"{command.upper()} is not a valid command!")
-            else:
-                self.handle_echo(command[4::].strip())
-
-        except TypeError:
-            print(f"The {command} command must have a parameter.")
-            
+        elif command.startswith("echo"):
+            self.commands[command[4::].strip()]
+        else:
+            print(f"{command.upper()} is not valid")
 commandInterpreter = CommandInterpreter()
 while commandInterpreter.isRunning:
     command = commandInterpreter.handle_input("> ").lower()
     commandInterpreter.validate_command(command)
-    
